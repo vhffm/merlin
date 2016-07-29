f5bb0df9b8ae50ce907dca4648ffce00098e3027
Martin Stone <martin@d7415.co.uk>
2016-07-30 00:21:51 +0100

    Change infrastructure tech levels for Round 68.
    (cherry picked from commit 4673b039a92380624e0aa4cf3dd2419f02acc3f1)

--- Core/maps.py
+++ Core/maps.py
@@ -1918,7 +1918,9 @@ class DevScan(Base):
         if level==3:
             return "150 constructions"
         if level==4:
-            return "250 constructions"
+            return "200 constructions"
+        if level==5:
+            return "300 constructions"
     
     def hulls_str(self):
         level = self.hulls
